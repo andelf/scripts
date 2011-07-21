ffce3fd7336cf788fc9cc32b1f70bf2c921892ca
Feather.et.ELF <andelf@gmail.com>
2011-07-21 21:26:53 +0800

    dummy msg

--- vm-monitor.sh
+++ vm-monitor.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+
+# configuration
+DURATION=$((60 * 60 * 2))
+INTERVAL=60
+
+# auto calculated
+SCRIPT_NAME=$(basename $0)
+
+DIR_NAME=$(dirname "$0")
+START_TIME=$(date +%s)
+
+#TEMP_FILE=$(mktemp)
+VMSTAT_PID=0
+
+
+vmstat 5 | tail -n 1 > $TEMP_FILE
+VMSTAT_PID=
